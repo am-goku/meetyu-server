f8bd52a5c4626274647e1f38037a752b9e53bfb1
Gokul Krishna <amkrishna2483@gmail.com>
2024-02-02 08:36:23 +0530

    chatroom schema completed

--- src/models/chatroom-schema.js
+++ src/models/chatroom-schema.js
@@ -1,16 +1,16 @@
 import mongoose, { Schema, model } from "mongoose";
 
-
-
 const chatRoomSchema = new Schema({
-    users: {
-        type: [
-            type: mongoose.Types.ObjectId,
-            require: true,
-            
-        ]
-    }
-})
+  users: {
+    type: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+    require: true,
+  },
 
+  last_message: {
+    type: mongoose.Types.ObjectId,
+    require: false,
+    ref: "message",
+  },
+});
 
-export default Chatroom = model('chatroom', chatRoomSchema)
+export default Chatroom = model("chatroom", chatRoomSchema);
