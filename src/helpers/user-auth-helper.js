af659f2eb18fa3cfb1b21a323e48cd4bec60c73f
Gokul Krishna <amkrishna2483@gmail.com>
2024-03-21 14:44:56 +0530

    changed all helper function to its prev form and updated it

--- src/helpers/user-auth-helper.js
+++ src/helpers/user-auth-helper.js
@@ -5,184 +5,168 @@ import { sign_access_token, sign_refresh_token } from "../services/jwt.js";
 import { sent_verification_mail } from "../services/nodemailer.js";
 import crypto from "crypto";
 
-const loginAndRegister = {
-  // @desc   User login
-  // @Route  POST /api/v1/user/login
-  // @access Public
-  login_helper: ({ email, password }) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const user = await User.findOne({ email: email });
+// @desc   User login
+// @Route  POST /api/v1/user/login
+// @access Public
+export const login_helper = ({ email, password }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({ email: email });
 
-        //Exceptions start here
-        if (!user) return reject({ status: 400, message: "User not found" });
+      //Exceptions start here
+      if (!user) return reject({ status: 400, message: "User not found" });
 
-        if (user.blocked)
-          return reject({ status: 403, message: "User has been blocked." });
+      if (user.blocked)
+        return reject({ status: 403, message: "User has been blocked." });
 
-        if (!user.emailVerified) {
-          const token = crypto.randomBytes(96).toString("hex"); //Generating token for sending authmail
-          await sent_verification_mail(user.email, user.name, token);
-          const newAuth = new Auth({ email, token });
-          await newAuth.save();
-          return reject({
-            status: 403,
-            message: "Email has not been verified yet.",
-            message2: "Please verify your email.",
-          });
-        }
-
-        //Password check
-        const passMatch = await compare_password(password, user?.password);
-
-        if (!passMatch)
-          return reject({ status: 400, message: "Wrong password" });
-
-        //Token creation and response
-        if (user && passMatch) {
-          const accessToken = await sign_access_token({
-            userId: user?._id,
-            email: user?.email,
-            role: user?.role,
-          }); //Fetch the access token
-          const refreshToken = await sign_refresh_token({
-            userId: user?._id,
-            email: user?.email,
-            role: user?.role,
-          }); //Fetching the refresh token
-
-          const transformedUser = user.toObject(); //Converting the mongoose document object to plain object
-          delete transformedUser.password; //deleining the password from user data
-
-          resolve({
-            status: 200,
-            accessToken,
-            refreshToken,
-            user: transformedUser,
-          });
-        }
-      } catch (error) {
-        reject({ status: 500, message: "Server error" });
-      }
-    });
-  },
-
-  // @desc   Register login
-  // @Route  POST /api/v1/user/register
-  // @access Public
-  register_helper: ({ name, username, email, pass, confPass }) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        if (pass !== confPass)
-          return reject({
-            status: 400,
-            message: "Password doesn't match.",
-          });
-
-        const user = await User.findOne({ email, username });
-
-        if (user) {
-          if (user.emailVerified) {
-            return reject({
-              status: 409,
-              message: "Email/Username has already been registerd.",
-            });
-          } else {
-            return reject({
-              status: 400,
-              message: "Account has already been registered, but not verified.",
-            });
-          }
-        }
-
+      if (!user.emailVerified) {
         const token = crypto.randomBytes(96).toString("hex"); //Generating token for sending authmail
-
-        const newUser = new User({
-          email,
-          username,
-          password: await hash_password(pass),
-          name,
-        });
+        await sent_verification_mail(user.email, user.name, token);
         const newAuth = new Auth({ email, token });
-
-        sent_verification_mail(email, name, token)
-          .then(async (response) => {
-            await newAuth.save();
-            await newUser.save();
-            resolve({ status: 200, ...response });
-          })
-          .catch((err) => {
-            console.log(err);
-            reject({ status: 500, ...err });
-          });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error });
+        await newAuth.save();
+        return reject({
+          status: 403,
+          message: "Email has not been verified yet.",
+          message2: "Please verify your email.",
+        });
       }
-    });
-  },
-};
 
-const authentication = {
-  // @desc   Email verification
-  // @Route  POST /api/v1/auth/:email/verify/:token
-  // @access Public
-  auth_helper: ({ token, email }) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const currentTime = new Date();
-        Auth.findOne({ email, token })
-          .then(async (auth) => {
-            const createdAt = auth.createdAt;
-            const diffInMs = currentTime - createdAt;
-            const diffInHrs = diffInMs / (1000 * 60 * 60);
+      //Password check
+      const passMatch = await compare_password(password, user?.password);
 
-            if (diffInHrs > 24) {
-              return reject({ status: 410, message: "Token has been expired" });
-            }
+      if (!passMatch) return reject({ status: 400, message: "Wrong password" });
 
-            await User.updateOne({ email }, { $set: { emailVerified: true } });
-            resolve({ status: 200, message: "Email has been verified." });
-          })
-          .catch((err) => {
-            reject({ status: 410, message: "Token is not valid", err });
-          });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error });
-      }
-    });
-  },
-};
-
-const tokenRel = {
-  // @desc   Token refresh
-  // @Route  POST /api/v1/auth/refresh-token
-  // @access Public
-  tokenRefresh_helper: (user) => {
-    return new Promise((resolve, reject) => {
-      try {
-        const newAccessToken = sign_access_token({
+      //Token creation and response
+      if (user && passMatch) {
+        const accessToken = await sign_access_token({
           userId: user?._id,
           email: user?.email,
           role: user?.role,
-        });
+        }); //Fetch the access token
+        const refreshToken = await sign_refresh_token({
+          userId: user?._id,
+          email: user?.email,
+          role: user?.role,
+        }); //Fetching the refresh token
 
-        return resolve({
+        const transformedUser = user.toObject(); //Converting the mongoose document object to plain object
+        delete transformedUser.password; //deleining the password from user data
+
+        resolve({
           status: 200,
-          message: "Created new access token",
-          newAccessToken,
+          accessToken,
+          refreshToken,
+          user: transformedUser,
         });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error });
       }
-    });
-  },
+    } catch (error) {
+      reject({ status: 500, message: "Server error" });
+    }
+  });
 };
 
+// @desc   Register login
+// @Route  POST /api/v1/user/register
+// @access Public
+export const register_helper = ({ name, username, email, pass, confPass }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (pass !== confPass)
+        return reject({
+          status: 400,
+          message: "Password doesn't match.",
+        });
 
+      const user = await User.findOne({ email, username });
 
-export const {
-  login_helper,
-  register_helper,
-  auth_helper,
-  tokenRefresh_helper,
-} = { ...loginAndRegister, ...authentication, ...tokenRel };
+      if (user) {
+        if (user.emailVerified) {
+          return reject({
+            status: 409,
+            message: "Email/Username has already been registerd.",
+          });
+        } else {
+          return reject({
+            status: 400,
+            message: "Account has already been registered, but not verified.",
+          });
+        }
+      }
+
+      const token = crypto.randomBytes(96).toString("hex"); //Generating token for sending authmail
+
+      const newUser = new User({
+        email,
+        username,
+        password: await hash_password(pass),
+        name,
+      });
+      const newAuth = new Auth({ email, token });
+
+      sent_verification_mail(email, name, token)
+        .then(async (response) => {
+          await newAuth.save();
+          await newUser.save();
+          resolve({ status: 200, ...response });
+        })
+        .catch((err) => {
+          console.log(err);
+          reject({ status: 500, ...err });
+        });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error", error });
+    }
+  });
+};
+
+// @desc   Email verification
+// @Route  POST /api/v1/auth/:email/verify/:token
+// @access Public
+export const auth_helper = ({ token, email }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const currentTime = new Date();
+      Auth.findOne({ email, token })
+        .then(async (auth) => {
+          const createdAt = auth.createdAt;
+          const diffInMs = currentTime - createdAt;
+          const diffInHrs = diffInMs / (1000 * 60 * 60);
+
+          if (diffInHrs > 24) {
+            return reject({ status: 410, message: "Token has been expired" });
+          }
+
+          await User.updateOne({ email }, { $set: { emailVerified: true } });
+          resolve({ status: 200, message: "Email has been verified." });
+        })
+        .catch((err) => {
+          reject({ status: 410, message: "Token is not valid", err });
+        });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error", error });
+    }
+  });
+};
+
+// @desc   Token refresh
+// @Route  POST /api/v1/auth/refresh-token
+// @access Public
+export const tokenRefresh_helper = (user) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const newAccessToken = sign_access_token({
+        userId: user?._id,
+        email: user?.email,
+        role: user?.role,
+      });
+
+      return resolve({
+        status: 200,
+        message: "Created new access token",
+        newAccessToken,
+      });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error", error });
+    }
+  });
+};
--- src/helpers/user-chat-helper.js
+++ src/helpers/user-chat-helper.js
@@ -1,278 +1,260 @@
 import { Chatroom } from "../models/chatroom-schema.js";
 
 //@desc   Chatroom CURD Operations
-const chatroom_operations = {
-  /*
+
+/*
     @desc   Create chat room for Single and group chats.
     @Route  POST /api/v1/chat/create-chatroom
     @access Protected - (Authenticated user)
   */
-  create_chatroom_hlpr: (userId, participants, room_name) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        participants.push(userId);
+export const create_chatroom_hlpr = (userId, participants, room_name) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      participants.push(userId);
 
-        if (participants.length < 3)
-          return reject({
-            status: 400,
-            message: "Atleast 3 participants are required.",
-          });
-
-        const newRoom = new Chatroom({
-          users: participants.sort(),
-          room_name,
-          createdBy: userId,
-          room_admins: [userId],
-          owner: userId,
+      if (participants.length < 3)
+        return reject({
+          status: 400,
+          message: "Atleast 3 participants are required.",
         });
 
-        await newRoom.save();
+      const newRoom = new Chatroom({
+        users: participants.sort(),
+        room_name,
+        createdBy: userId,
+        room_admins: [userId],
+        owner: userId,
+      });
 
-        resolve({ status: 201, message: "Chat room created successfully." });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error });
-      }
-    });
-  },
+      await newRoom.save();
 
-  /*
+      resolve({ status: 201, message: "Chat room created successfully." });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error", error });
+    }
+  });
+};
+
+/*
     @desc   Get char tooms of a particular user.
     @Route  GET /api/v1/chat/get-chatrooms
     @access Protected - (Authenticated user)
   */
-  get_chatrooms_hlpr: (userId) => {
-    return new Promise((resolve, reject) => {
-      try {
-        Chatroom.aggregate([
-          { $match: { users: { $in: [userId] } } },
-          { $sort: { updatedAt: -1 } },
-        ])
-          .then((chatRooms) => {
-            resolve({ status: 200, rooms: chatRooms });
-          })
-          .catch((err) => {
-            reject({ status: 500, message: "Database error", error: err });
-          });
-      } catch (error) {
-        reject({ status: 400, message: "Internal server error", error });
-      }
-    });
-  },
+export const get_chatrooms_hlpr = (userId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      Chatroom.aggregate([
+        { $match: { users: { $in: [userId] } } },
+        { $sort: { updatedAt: -1 } },
+      ])
+        .then((chatRooms) => {
+          resolve({ status: 200, rooms: chatRooms });
+        })
+        .catch((err) => {
+          reject({ status: 500, message: "Database error", error: err });
+        });
+    } catch (error) {
+      reject({ status: 400, message: "Internal server error", error });
+    }
+  });
+};
 
-  /*
+/*
     @desc   Delete a chat room.
     @Route  DELETE /api/v1/chat/delete-chatrooms 
     @Body   {roomId, userId}
     @access Protected - (Authenticated user)
   */
-  delete_chatrooms_hlpr: (roomId, userId) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, owner: userId },
-          { $set: { deleted: true } }
-        )
-          .then((res) => {
-            resolve({ status: 200, message: "Room deleted successfully" });
-          })
-          .catch((err) => {
-            reject({ status: 500, message: "Error deleting chatroom." });
-          });
-      } catch (error) {
-        reject({ status: 400, message: "Internal server error", error });
-      }
-    });
-  },
+export const delete_chatrooms_hlpr = (roomId, userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, owner: userId },
+        { $set: { deleted: true } }
+      )
+        .then((res) => {
+          resolve({ status: 200, message: "Room deleted successfully" });
+        })
+        .catch((err) => {
+          reject({ status: 500, message: "Error deleting chatroom." });
+        });
+    } catch (error) {
+      reject({ status: 400, message: "Internal server error", error });
+    }
+  });
+};
 
-  /*
+/*
     @desc   Update a chat room.
     @Route  PUT /api/v1/chat/update-chatrooms/:roomId
     @Body   {room_name, background, icon}
     @access Protected - (Authenticated user)
   */
-  update_chatroom_hlpr: (roomId, userId, data) => {
-    return new Promise((resolve, reject) => {
-      try {
-        const { room_name, background, icon } = data;
+export const update_chatroom_hlpr = (roomId, userId, data) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const { room_name, background, icon } = data;
 
-        const query = {};
-        if (room_name) query["room_name"] = room_name;
-        if (background) query["background"] = background;
-        if (icon) query["icon"] = icon;
+      const query = {};
+      if (room_name) query["room_name"] = room_name;
+      if (background) query["background"] = background;
+      if (icon) query["icon"] = icon;
 
-        Chatroom.updateOne(
-          { _id: roomId, room_admins: { $in: [userId] } },
-          { $set: query }
-        )
-          .then((response) => {
-            resolve({ status: 201, message: "Chatroom updated successfully." });
-          })
-          .catch((error) => {
-            reject({ status: 400, message: "Error updating chatroom." });
-          });
-      } catch (error) {
-        reject({ status: 500, message: "Error updating chatroom" });
-      }
-    });
-  },
+      Chatroom.updateOne(
+        { _id: roomId, room_admins: { $in: [userId] } },
+        { $set: query }
+      )
+        .then((response) => {
+          resolve({ status: 201, message: "Chatroom updated successfully." });
+        })
+        .catch((error) => {
+          reject({ status: 400, message: "Error updating chatroom." });
+        });
+    } catch (error) {
+      reject({ status: 500, message: "Error updating chatroom" });
+    }
+  });
 };
 
 //@desc   Chatroom Ownership Operations
-const chatroom_ownership = {
-  /*
+/*
     @desc   Remove an admin from a chat room.
     @Route  DELETE /api/v1/chat/remove-admin 
     @Body   {roomId, adminId, userId}
     @access Protected - (Authenticated user)
   */
-  remove_admin_hlpr: (roomId, adminId, userId) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, owner: userId },
-          { $pull: { room_admins: adminId } }
-        )
-          .then((response) => {
-            resolve({ status: 200, message: "Admin removed successfully." });
-          })
-          .catch((err) => {
-            reject({
-              status: 500,
-              message: "Error removing admin",
-              error: err,
-            });
+export const remove_admin_hlpr = (roomId, adminId, userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, owner: userId },
+        { $pull: { room_admins: adminId } }
+      )
+        .then((response) => {
+          resolve({ status: 200, message: "Admin removed successfully." });
+        })
+        .catch((err) => {
+          reject({
+            status: 500,
+            message: "Error removing admin",
+            error: err,
           });
-      } catch (error) {
-        reject({ status: 400, message: "Internal server error", error });
-      }
-    });
-  },
+        });
+    } catch (error) {
+      reject({ status: 400, message: "Internal server error", error });
+    }
+  });
+};
 
-  /*
+/*
     @desc   Add new admin to a chat room.
     @Route  POST /api/v1/chat/add-admin 
     @Body   {roomId, adminId, userId}
     @access Protected - (Authenticated user)
   */
-  add_admin_hlpr: (roomId, userId, adminId) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, owner: userId },
-          { $addToSet: { room_admins: adminId } }
-        )
-          .then((response) => {
-            resolve({ status: 200, message: "Admin added successfully" });
-          })
-          .catch((error) => {
-            reject({
-              status: 400,
-              message: "Error adding admin.",
-              error: error,
-            });
+export const add_admin_hlpr = (roomId, userId, adminId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, owner: userId },
+        { $addToSet: { room_admins: adminId } }
+      )
+        .then((response) => {
+          resolve({ status: 200, message: "Admin added successfully" });
+        })
+        .catch((error) => {
+          reject({
+            status: 400,
+            message: "Error adding admin.",
+            error: error,
           });
-      } catch (error) {
-        reject({ status: 400, message: "Internal server error", error });
-      }
-    });
-  },
+        });
+    } catch (error) {
+      reject({ status: 400, message: "Internal server error", error });
+    }
+  });
+};
 
-  /*
+/*
     @desc   Change chatroom ownership.
     @Route  PATCH /api/v1/chat/change-owner/:roomId
     @Body   {roomId, userId, adminId}
     @access Protected - (Authenticated user)
   */
-  change_owner_hlpr: (roomId, userId, adminId) => {
-    return new Promise((resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, owner: userId },
-          { $set: { owner: adminId } },
-          { new: true }
-        )
-          .then((chatroom) => {
-            resolve({
-              status: 200,
-              message: "Owner has been updated.",
-              chatroom,
-            });
-          })
-          .catch((error) => {
-            reject({ status: 400, message: "Error updating owner.", error });
+export const change_owner_hlpr = (roomId, userId, adminId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, owner: userId },
+        { $set: { owner: adminId } },
+        { new: true }
+      )
+        .then((chatroom) => {
+          resolve({
+            status: 200,
+            message: "Owner has been updated.",
+            chatroom,
           });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error.", error });
-      }
-    });
-  },
+        })
+        .catch((error) => {
+          reject({ status: 400, message: "Error updating owner.", error });
+        });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error.", error });
+    }
+  });
 };
-
 //@desc   Chatroom Ownership Operations
-const chatroom_user_managemet = {
-  /*
+/*
     @desc   Add new user/users to chat room.
     @Route  POST /api/v1/chat/add-users/:roomId
     @Body   {participants:[Array]}
     @access Protected - (Authenticated user)
   */
-  add_user_hlpr: (roomId, userId, participants) => {
-    return new Promise((resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, room_admins: { $in: [userId] } },
-          { $addToSet: { users: { $each: participants } } }
-        )
-          .then(() => {
-            resolve({ status: 200, message: "User added successfully" });
-          })
-          .catch(() => {
-            reject({ status: 400, message: "Error adding user" });
-          });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error: error });
-      }
-    });
-  },
+export const add_user_hlpr = (roomId, userId, participants) => {
+  return new Promise((resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, room_admins: { $in: [userId] } },
+        { $addToSet: { users: { $each: participants } } }
+      )
+        .then(() => {
+          resolve({ status: 200, message: "User added successfully" });
+        })
+        .catch(() => {
+          reject({ status: 400, message: "Error adding user" });
+        });
+    } catch (error) {
+      reject({
+        status: 500,
+        message: "Internal server error",
+        error: error,
+      });
+    }
+  });
+};
 
-  /*
+/*
     @desc   Delete user/users from chat room.
     @Route  DELETE /api/v1/chat/remove-users/:roomId
     @Body   {participants:[Array]}
     @access Protected - (Authenticated user)
   */
-  remove_user_hlpr: (roomId, userId, participants) => {
-    return new Promise((resolve, reject) => {
-      try {
-        Chatroom.updateOne(
-          { _id: roomId, room_admins: { $in: [userId] } },
-          { $pull: { users: { $each: participants } } }
-        )
-          .then(() => {
-            resolve({ status: 200, message: "User has been removed." });
-          })
-          .catch(() => {
-            reject({ status: 400, message: "Error removing user." });
-          });
-      } catch (error) {
-        reject({ status: 500, message: "Internal server error", error: error });
-      }
-    });
-  },
-};
-
-export const {
-  create_chatroom_hlpr,
-  get_chatrooms_hlpr,
-  delete_chatrooms_hlpr,
-  update_chatroom_hlpr,
-  remove_admin_hlpr,
-  add_admin_hlpr,
-  change_owner_hlpr,
-  add_user_hlpr,
-  remove_user_hlpr,
-} = {
-  ...chatroom_operations,
-  ...chatroom_ownership,
-  ...chatroom_user_managemet,
+export const remove_user_hlpr = (roomId, userId, participants) => {
+  return new Promise((resolve, reject) => {
+    try {
+      Chatroom.updateOne(
+        { _id: roomId, room_admins: { $in: [userId] } },
+        { $pull: { users: { $each: participants } } }
+      )
+        .then(() => {
+          resolve({ status: 200, message: "User has been removed." });
+        })
+        .catch(() => {
+          reject({ status: 400, message: "Error removing user." });
+        });
+    } catch (error) {
+      reject({ status: 500, message: "Internal server error", error: error });
+    }
+  });
 };
